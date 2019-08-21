7615e1c52e62c2f9d553d738e792f81a75a93a1b
SoyeonKang <aaa0311@naver.com>
2019-08-21 17:53:39 +0900

    0821 commit

--- src/Study0819week/BaekJ14889_스타트와링크.java
+++ src/Study0819week/BaekJ14889_스타트와링크.java
@@ -1,9 +1,23 @@
 package Study0819week;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BaekJ14889_스타트와링크 {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(bf.readLine());
+		StringTokenizer tk;
+		int[][] arr = new int[N][N];
+		for(int i=0; i<N; i++) {
+			tk = new StringTokenizer(bf.readLine());
+			for(int j=0; j<N; j++) {
+				arr[i][j]=Integer.parseInt(tk.nextToken());
+			}
+		}
 	}
 
 }
