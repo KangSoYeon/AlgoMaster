99e06414a9cb691d3050346b99583c4153abdafc
SoyeonKang <aaa0311@naver.com>
2019-10-04 17:19:51 +0900

    sdfs

---
+++ src/first10/BaekJ1654_랜선자르기.java
@@ -0,0 +1,18 @@
+package first10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJ1654_랜선자르기 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer tk = new StringTokenizer(bf.readLine());
+		int K = Integer.parseInt(tk.nextToken());
+		int N = Integer.parseInt(tk.nextToken());
+
+	}
+
+}
---
+++ src/first10/Solution14002_가장긴증가하는부분수열4.java
@@ -0,0 +1,10 @@
+package first10;
+
+public class Solution14002_가장긴증가하는부분수열4 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
