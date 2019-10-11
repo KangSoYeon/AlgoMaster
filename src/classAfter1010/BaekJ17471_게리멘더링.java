e9dbcf21c055b1eaff4acfb3fa29381a3bc6188f
SoyeonKang <aaa0311@naver.com>
2019-10-11 16:15:45 +0900

    ㄴㅇㄹㄴㅇ

--- src/classAfter1010/BaekJ17471_게리멘더링.java
+++ src/classAfter1010/BaekJ17471_게리멘더링.java
@@ -7,12 +7,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-
 public class BaekJ17471_게리멘더링 {
 	static int N;
 	static int[] arr;
 	static int[] people;
 	static int[][] connect;
+	static ArrayList<Integer> e1;
+	static ArrayList<Integer> e2;
+	static boolean[] check;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -31,18 +33,18 @@ public class BaekJ17471_게리멘더링 {
 			tk = new StringTokenizer(bf.readLine());
 			int t = Integer.parseInt(tk.nextToken());
 			for(int j=0; j<t; j++) {
-				connect[i][j]= Integer.parseInt(tk.nextToken());
+				int temp = Integer.parseInt(tk.nextToken())-1;
+				connect[i][temp] = 1 ;
+				connect[temp][i] = 1;
 			}
 		}
-		
+		int min = 200;
+		boolean flag = false;
+		int cc =0;
 		
 		do {
-			//System.out.println(Arrays.toString(arr));
-			
-			ArrayList<Integer> e1 = new ArrayList<>(); //첫번째 구역
-			ArrayList<Integer> e2 = new ArrayList<>(); //두번쨰 구역
-			
-			for(int t=1; t<N; t++) {
+			cc++;
+			for(int t=1; t<(N/2)+1; t++) {
 				
 				e1 = new ArrayList<>(); //첫번째 구역
 				e2 = new ArrayList<>(); //두번쨰 구역
@@ -53,26 +55,64 @@ public class BaekJ17471_게리멘더링 {
 				for(int j=t; j<N; j++) {
 					e2.add(arr[j]);
 				}
+				int p1=0, p2=0;
 				
-				//System.out.println(e1);
-				//System.out.println(e2);
-				//첫번째 구역 연결 테스트 --> bfs
-				//두번쨰 구역 연결 테스트 
+				check = new boolean[N];
+				flag = false;
+				dfs1(e1.get(0));
+				for(int i=0; i<e1.size(); i++) {
+					if(!check[e1.get(i)]) { //하나라도 false이면 끝내기, 하나라도 방문 안했으면 
+						flag = true;
+					}
+					p1 += people[e1.get(i)];
+				}
 				
+				if(flag) continue; //다음 케이스로 넘어가기 
 				
+				check = new boolean[N];
+				flag = false;
+				dfs2(e2.get(0));
+				for(int i=0; i<e2.size(); i++) {
+					if(!check[e2.get(i)]) {
+						flag = true;
+					}
+					p2 += people[e2.get(i)];
+				}
+				
+				if(flag) continue;
+
+				//Math.abs(p1-p2)의 최소값 구하기 
+				if(min>Math.abs(p1-p2)) {
+					min = Math.abs(p1-p2);
+				}
 			}
 			
-			
-			
 		}while(next_permutation());
+		
+		if(min != 200) System.out.println(min);
+		else System.out.println(-1);
 		
 	}
 	
 	//e1에 있는 배열들 서로 연결되어 있는지 확인하는 함수 
+	static void dfs1(int n) {
+		for(int i=0; i<N; i++) {
+			if(e1.contains(i) && connect[n][i]==1 && !check[i]) { //n과 연결되어 있고, check간적 없으면 
+				check[i] = true;
+				dfs1(i); //여기도 간다 
+			}
+		}		
+	}
 	
-	
-	
-	
+	static void dfs2(int n) {
+		for(int i=0; i<N; i++) {
+			if(e2.contains(i) && connect[n][i]==1 && !check[i]) { //n과 연결되어 있고, check간적 없으면 
+				check[i] = true;
+				dfs2(i); //여기도 간다 
+			}
+		}		
+	}
+
 	static boolean next_permutation() {
 		int i= N-1;
 		while(i>0 && arr[i] <= arr[i-1]) i--;
---
+++ src/classAfter1010/Main_B17471_게리맨더링_김태희_np.java
@@ -0,0 +1,149 @@
+package classAfter1010;
+
+
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main_B17471_게리맨더링_김태희_np {
+
+	static int N,half,countA,countB,pA,pB,visited ,P[],MIN;
+	static int[][] map;
+	static ArrayList<Integer> aList,bList;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		N = Integer.parseInt(in.readLine());
+		half = N/2;
+		map = new int[N][N];
+		P = new int[N];
+		MIN = Integer.MAX_VALUE;
+		
+		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			P[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = 0,j=0; i < N; i++) {
+			st = new StringTokenizer(in.readLine(), " ");
+			int cnt =  Integer.parseInt(st.nextToken());
+			for (int c = 0; c < cnt; c++) {
+				j =  Integer.parseInt(st.nextToken())-1;
+				map[j][i] = map[i][j] = 1;
+			}
+		}
+		
+		for (int i = 1; i <= half; i++) {
+			divide(i);
+		}
+		System.out.println(MIN==Integer.MAX_VALUE?-1:MIN);
+	}
+
+	static int np[];
+	private static void divide(int target) {
+		np = new int[N];
+		int c = 0,i=N-1;
+		while(c++<target) np[i--] = 1;
+		
+		do {
+			compare();			
+		}while(nextPermutation());
+		
+	}
+
+	private static boolean nextPermutation() {
+		
+		int i = N-1;
+		while(i>0 && np[i-1]>= np[i]) --i;
+		
+		if(i==0) return false;
+		
+		int j = N-1;
+		while (np[i-1] >= np[j]) --j;
+		
+		int temp = np[i-1];
+		np[i-1] = np[j];
+		np[j] = temp;
+		
+		j = N-1;
+		while(i<j) {
+			temp = np[i];
+			np[i] = np[j];
+			np[j] = temp;
+			++i; --j;
+		}
+		return true;
+	}
+	
+	private static void compare() {
+		aList = new ArrayList<Integer>();
+		bList = new ArrayList<Integer>();
+		for (int i = 0; i < N; i++) {
+			if(np[i]>0) aList.add(i);
+			else	bList.add(i);
+		}
+		countA = countB = pA = pB = 0;
+		visited = 0;
+		dfs(aList,aList.get(0),true);
+//		bfs(aList,true);
+		if(countA != aList.size()) return;
+		
+		visited = 0;
+		dfs(bList,bList.get(0),false);
+//		bfs(bList,false);
+		if(countB != bList.size()) return;
+		
+		if(MIN > Math.abs(pA - pB)) MIN = Math.abs(pA - pB);
+		
+	}
+
+	private static void dfs(ArrayList<Integer> list,int v,boolean type) {
+		visited |= 1<<v;
+		if(type) {
+			countA++;
+			pA += P[v];
+		}else {
+			countB++;
+			pB += P[v];
+		}
+		
+		for (int i = 0; i < N; i++) {
+			if(list.contains(i) && map[v][i]>0 && (visited&1<<i)==0) {
+				dfs(list,i,type);
+			}
+		}
+	}
+	private static void bfs(ArrayList<Integer> list,boolean type) {
+		
+		Queue<Integer> queue = new LinkedList<Integer>();
+		int start = list.get(0);
+		queue.offer(start);
+		visited |= 1<<start;
+		
+		while(!queue.isEmpty()) {
+			int v = queue.poll();
+			if(type) {
+				countA++;
+				pA += P[v];
+			}else {
+				countB++;
+				pB += P[v];
+			}
+			
+			for (int i = 0; i < N; i++) {
+				if(list.contains(i) && map[v][i]>0 && (visited&1<<i)==0) {
+					visited |= 1<<i;
+					queue.offer(i);
+				}
+			}
+		}
+	}
+}
